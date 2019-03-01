0729144936df2c30ff3b5575a7e4d84ca8294630
Tony Hansen <THansenite@gmail.com>
2019-02-28 20:06:13 -0600

    More Xref constants

--- ApopheniaScraper/Constants/StatScraperConst.cs
+++ ApopheniaScraper/Constants/StatScraperConst.cs
@@ -8,7 +8,16 @@ namespace ApopheniaScraper.Constants
 {
     public class StatScraperConst
     {
+        // Game Data
+        public const string GAME_DATE = "//*[@id=\"game_date\"]";
+        public const string GAME_TIME = "//*[@id=\"game_time\"]";
         public const string HOME_TEAM = "//*[@id=\"home_team_name\"]";
         public const string AWAY_TEAM = "//*[@id=\"away_team_name\"]";
+
+        //Final Data
+        public const string REFEREES = "//*[@id=\"game_refs\"]";
+        public const string HOME_SCORE = "//*[@id=\"home_team_score\"]";
+        public const string AWAY_SCORE = "//*[@id=\"away_team_score\"]";
+		
     }
 }
--- ApopheniaScraper/Parsers/StatSheetParser.cs
+++ ApopheniaScraper/Parsers/StatSheetParser.cs
@@ -14,12 +14,25 @@ namespace ApopheniaScraper.Parsers
             HtmlDocument pageDocument = new HtmlDocument();
             pageDocument.LoadHtml(scrapedText);
 
-            var homeTeam = pageDocument.DocumentNode.SelectSingleNode(Constants.StatScraperConst.HOME_TEAM).InnerText;
-            var awayTeam = pageDocument.DocumentNode.SelectSingleNode(Constants.StatScraperConst.AWAY_TEAM).InnerText;
+            var gameDate = GetXrefData(pageDocument, Constants.StatScraperConst.GAME_DATE);
+            var gameTime = GetXrefData(pageDocument, Constants.StatScraperConst.GAME_TIME);
+            var homeTeam = GetXrefData(pageDocument, Constants.StatScraperConst.HOME_TEAM);
+            var awayTeam = GetXrefData(pageDocument, Constants.StatScraperConst.AWAY_TEAM);
 
-            Console.WriteLine("Home Team: " + homeTeam);
-            Console.WriteLine("Away Team: " + awayTeam);
+            var homeScore = GetXrefData(pageDocument, Constants.StatScraperConst.HOME_SCORE);
+            var awayScore = GetXrefData(pageDocument, Constants.StatScraperConst.AWAY_SCORE);
+            var refs = GetXrefData(pageDocument, Constants.StatScraperConst.REFEREES);
+
+            Console.WriteLine("Schedule:  " + gameDate + " " + gameTime);
+            Console.WriteLine("Home Team: " + homeTeam + " - " + homeScore);
+            Console.WriteLine("Away Team: " + awayTeam + " - " + awayScore);
+            Console.WriteLine("Referees:  " + refs);
             Console.ReadKey();
+        }
+
+        private string GetXrefData(HtmlDocument docText, string xrefConst)
+        {
+            return docText.DocumentNode.SelectSingleNode(xrefConst).InnerText;
         }
     }
 }
